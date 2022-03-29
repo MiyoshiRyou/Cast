076ff8500fdfac48ab6d8ffb44be48476f4adb08
miyoshiryou <miyoshiryou@192.168.1.24>
2022-03-29 11:32:25 +0900

    First commit

---
+++ src/sample/Cast.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class Cast {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		short s = 1000;
+		byte b = (byte)s;
+		System.out.println(b);
+		
+		double d = 1.0/3;
+		System.out.println(d);
+		
+		float f = (float)d;
+		System.out.println(f);
+		
+		float f2 = 3.1415f;
+		int i = (int)f2;
+		System.out.println(i);
+	}
+
+}
